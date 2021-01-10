c22acccc6a99a4dd89977269e1bbe1cd476d4dc1
Ulrich Kilian <uk@science-and-more.de>
2021-01-10 23:17:13 +0100

    corrected settings

--- eventmanager/wsgi.py
+++ eventmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.base')
 
 application = get_wsgi_application()
