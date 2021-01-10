648a2daff73407c2270734e730a2c9d5a2fd667f
Ulrich Kilian <uk@science-and-more.de>
2021-01-11 00:14:38 +0100

    error in wsgi

--- eventmanager/wsgi.py
+++ eventmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.production')
 
 application = get_wsgi_application()
