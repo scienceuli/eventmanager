73a97221ed1f0596a1a43c4d1120334920e6dc75
Ulrich Kilian <uk@science-and-more.de>
2021-01-13 14:09:00 +0100

    settings organized

--- eventmanager/celery.py
+++ eventmanager/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.local_settings')
 
 app = Celery('eventmanager')
 
--- eventmanager/wsgi.py
+++ eventmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanager.local_settings')
 
 application = get_wsgi_application()
