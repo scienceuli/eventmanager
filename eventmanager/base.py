1b441f8b03b534398ae6751f45a4134b7155e273
Ulrich Kilian <scienceuli@Air-von-Ulrich.fritz.box>
2021-03-25 08:50:30 +0100

    minor changes

--- eventmanager/base.py
+++ eventmanager/base.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'jet.dashboard',
-    #'jet',
-    'grappelli',
+    'jet.dashboard',
+    'jet',
+    #'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
