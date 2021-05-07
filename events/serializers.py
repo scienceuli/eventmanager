5b5783da6545a004ad904779ee5a406c31e14d51
Ulrich Kilian <scienceuli@Air-von-Ulrich.fritz.box>
2021-05-07 06:15:14 +0200

    added get absolute url to serializer

--- events/serializers.py
+++ events/serializers.py
@@ -7,12 +7,15 @@ class SpeakerSerializer(serializers.ModelSerializer):
         model = EventSpeaker
         fields = ('first_name', 'last_name')
 
+
+
 class EventSerializer(serializers.ModelSerializer):
 
     speaker = SpeakerSerializer(read_only=True, many=True)
     category_name = serializers.CharField(source='category.name')
     first_day = serializers.DateField(format="%d.%m.%Y")
+    event_absolute_url = serializers.URLField(source='get_absolute_url', read_only=True)
 
     class Meta:
         model = Event
-        fields = ('id','category_name', 'name', 'first_day', 'description', 'speaker',)
+        fields = ('id','category_name', 'name', 'first_day', 'event_absolute_url', 'description', 'speaker',)
