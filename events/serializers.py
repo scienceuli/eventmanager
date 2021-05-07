958a6bb10e5d0c44bd1f6fe0c9c013948d376eaf
Ulrich Kilian <scienceuli@Air-von-Ulrich.fritz.box>
2021-05-07 07:11:57 +0200

    added web url to serializer

--- events/serializers.py
+++ events/serializers.py
@@ -13,12 +13,8 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     speaker = SpeakerSerializer(read_only=True, many=True)
     category_name = serializers.CharField(source='category.name')
     first_day = serializers.DateField(format="%d.%m.%Y")
-    web_url = serializers.SerializerMethodField()
-
-    def get_web_url(self, obj):
-        obj_url = obj.get_absolute_url()
-        return self.context["request"].build_absolute_uri(obj_url)
+    url = serializers.HyperlinkedIdentityField(view_name='event-detail', read_only=True)
 
     class Meta:
         model = Event
-        fields = ('id', 'category_name', 'name', 'first_day', 'web_url', 'description', 'speaker',)
+        fields = ('id', 'url', 'category_name', 'name', 'first_day', 'description', 'speaker',)
