d9520e9d071417540f5ec543f6e0c04f471e4c79
Ulrich Kilian <scienceuli@Air-von-Ulrich.fritz.box>
2021-05-07 07:47:43 +0200

    added url to serializer

--- events/serializers.py
+++ events/serializers.py
@@ -13,7 +13,7 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     speaker = SpeakerSerializer(read_only=True, many=True)
     category_name = serializers.CharField(source='category.name')
     first_day = serializers.DateField(format="%d.%m.%Y")
-    url = serializers.HyperlinkedIdentityField(view_name='event-detail', read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name='events:event-detail', read_only=True)
 
     class Meta:
         model = Event
