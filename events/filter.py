a0168aede51fb0fe8aa9a7cb3d05e4f8ca27c808
Ulrich Kilian <scienceuli@Air-von-Ulrich.fritz.box>
2021-05-13 08:42:35 +0200

    filter improved

--- events/filter.py
+++ events/filter.py
@@ -4,13 +4,21 @@ from events.models import Event, EventCategory
 
 class EventFilter(django_filters.FilterSet):
 
-    category = django_filters.ModelChoiceFilter(queryset=EventCategory.objects.all())
+    category = django_filters.ModelChoiceFilter(
+        queryset=EventCategory.objects.all(),
+        empty_label="Alle",
+    )
 
     first_day = django_filters.DateFromToRangeFilter(
-        label="Datumsbereich",
+        label="Datum eingrenzen",
         field_name="first_day",
         lookup_expr="month",
-        widget=django_filters.widgets.RangeWidget(attrs={"type": "date"}),
+        widget=django_filters.widgets.RangeWidget(
+            attrs={
+                "type": "date",
+                "class": "w-1/3 p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner",
+            }
+        ),
     )
 
     class Meta:
