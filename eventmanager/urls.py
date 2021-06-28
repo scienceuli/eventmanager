c9273e745bca6b3d6bb79de7fb70324df00f6e5c
Ulrich Kilian <scienceuli@Air-von-Ulrich.fritz.box>
2021-06-28 16:08:42 +0200

    disable self registration, bug fix char field length validation

--- eventmanager/urls.py
+++ eventmanager/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', user_views.login_page, name='login'),
     path('logout/', user_views.logout_page, name='logout'),
-    path('register/',user_views.register,name='register'),
+    # path('register/',user_views.register,name='register'),
     path('', include('events.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('<int:event_id>/pdf/', admin_event_pdf, name='admin-event-pdf'),
