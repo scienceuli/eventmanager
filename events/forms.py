79767e3640b25f3958c5fb739404785161dacbfa
Ulrich Kilian <scienceuli@Air-von-Ulrich.fritz.box>
2021-08-27 10:16:42 +0200

    bug fix free text field

--- events/forms.py
+++ events/forms.py
@@ -153,7 +153,8 @@ class EventMemberForm(forms.Form):
             attrs={
                 "class": "block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
             }
-        )
+        ),
+        required=False,
     )
 
     check = forms.BooleanField(
