db37c3d6dd98bd0aa90d4e6812c6c4db5d270d65
scienceuli <uk@science-and-more.de>
2025-02-14 11:23:47 +0100

    bug fix

--- events/models.py
+++ events/models.py
@@ -845,7 +845,7 @@ class Event(BaseModel, HitCountMixin):
     def get_balance(self):
         if not self.costs:
             return round(Decimal(0), 2)
-        balance = self.get_total_income_for_event() - self.cost
+        balance = self.get_total_income_for_event() - self.costs
         return balance
 
     def get_total_income_for_event(self):
