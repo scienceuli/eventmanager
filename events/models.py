4df8643252f3dda3d251081b2c30ee3cccbc29ab
scienceuli <uk@science-and-more.de>
2026-05-03 09:13:51 +0200

    fix: large about us field in admin

--- events/models.py
+++ events/models.py
@@ -70,7 +70,7 @@ class Home(BaseModel):
     impressum = RichTextField("Impressum", blank=True, config_name="short")
     legals = RichTextField("Rechtliche Hinweise", blank=True, config_name="short")
     privacy = RichTextField("Datenschutz", blank=True, config_name="short")
-    about_us = RichTextField("Über uns", blank=True, config_name="short")
+    about_us = RichTextField("Über uns", blank=True)
     nl_title = models.CharField(
         "Titel NL Signup", max_length=255, default="Bleiben Sie auf dem Laufenden"
     )
